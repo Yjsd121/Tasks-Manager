5eda4b9b5c1c6778dc70d45dbaffa2cfaaa5e463
Yader Sáenz <yadetjsd@gmail.com>
2026-06-09 21:34:26 -0600

    Token saved in localstorage, created middleware to verify token

--- frontend/src/login/login.jsx
+++ frontend/src/login/login.jsx
@@ -1,38 +1,7 @@
+import { Formlogin } from './Forms/loginform'
 import './login.css'
-import MailOutlineOutlinedIcon from '@mui/icons-material/MailOutlineOutlined'
-import VpnKeyOutlinedIcon from '@mui/icons-material/VpnKeyOutlined'
-import { useState } from 'react'
 
 export function Login() {
-  const [formData, setformData] = useState({
-    email: '',
-    password: ''
-  })
-
-  function handlechange(e) {
-    setformData({
-      ...formData,
-      [e.target.name]: e.target.value
-    })
-  }
-
-  async function handlesubmit(e) {
-    e.preventDefault()
-
-    const response = await fetch('http://localhost:3000/Auth/login', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-        email: formData.email,
-        password: formData.password
-      })
-    })
-
-    const token = await response.json()
-    console.log(token)
-  }
   return (
     <main className='principal'>
       <div style={{
@@ -45,39 +14,7 @@ export function Login() {
       </div>
 
       <h2>Gestor de tareas</h2>
-      <form
-        className='login-container'
-        onSubmit={handlesubmit}
-      >
-        <div className='login'>
-          <label>User</label>
-          <div className='input-style'>
-            <MailOutlineOutlinedIcon className='icon' />
-            <input
-              type='email'
-              name='email'
-              value={formData.email}
-              onChange={handlechange}
-              className='login-input'
-              placeholder='email'
-            />
-          </div>
-          <label>Password</label>
-          <div className='input-style'>
-            <VpnKeyOutlinedIcon className='icon' />
-            <input
-              type='password'
-              name='password'
-              value={formData.password}
-              onChange={handlechange}
-              className='login-input'
-              placeholder='password'
-            />
-          </div>
-        </div>
-        <button type='submit' className='primary-button'>Login</button>
-      </form>
-
+      <Formlogin />
     </main>
   )
 }
